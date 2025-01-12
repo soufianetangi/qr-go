61fee912d3903f72823e35d5ebf30be31cb8e4c4
soufianetangi <soufianetangi2@gmail.com>
2025-01-12 21:38:47 +0100

    Updated program

--- src/app/firebase/config.ts
+++ src/app/firebase/config.ts
@@ -7,7 +7,7 @@ import "firebase/firestore";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey:AIzaSyDH8G4Te5sCUOcfoqz5zrsXRLgNlii6HS0,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
