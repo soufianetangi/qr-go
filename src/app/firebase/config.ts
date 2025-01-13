860be1eeaf2dcdeb559fe6eabc29ead2a5aadc80
soufianetangi <soufianetangi2@gmail.com>
2025-01-13 21:03:00 +0100

    Updated program

--- src/app/firebase/config.ts
+++ src/app/firebase/config.ts
@@ -1,30 +1,18 @@
-// Import necessary Firebase modules and types
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import firebase from "firebase/app";
-import "firebase/firestore";
 
-// Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
-// Initializing Firebase app
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-
-// Initializing Firebase Auth and Firestore
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app); // Using getFirestore function
+const db = getFirestore(app);
 
-export {
-  app,
-  auth,
-  db
-};
+export { auth, db };
